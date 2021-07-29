ab16770bb1b64238bf141a279b2629b5d5e4d0c7
bnnm <bananaman255@gmail.com>
2021-07-29 15:23:19 +0200

    scripts: fix wwise-cleaner-bnk.py leftover debug

--- scripts/wwise-cleaner-bnk.py
+++ scripts/wwise-cleaner-bnk.py
@@ -94,9 +94,9 @@ def main():
             file_unwanted = os.path.join(move_dir, file_base)
 
             try:
-                #os.makedirs(os.path.dirname(file_unwanted), exist_ok=True)
-                #os.rename(file_base, file_unwanted)
-                print(file_base, file_unwanted)
+                os.makedirs(os.path.dirname(file_unwanted), exist_ok=True)
+                os.rename(file_base, file_unwanted)
+                #print(file_base, file_unwanted)
                 count += 1
             except:
                 pass #ignore not companion
