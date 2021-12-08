59a02b4427cb8e04546b98e96a75e41de261a82a
bnnm <bananaman255@gmail.com>
2021-12-09 00:22:40 +0100

    script: strings2 output cleanup helper

---
+++ scripts/txt-cleaner.py
@@ -0,0 +1,91 @@
+# Cleans text files from strings2.exe garbage
+
+import os, sys, itertools, re
+
+_PATTERN_WRONG = re.compile(r'[\t.<>,;.:{}\[\]()\'"$&/=!\\/#@+\^`´¨?|~*%]')
+_WORD_ALLOWED = ['xiii', 'xviii','zzz']
+_BAD_GROUPS = ['uu', 'fwfw','ldlD', 'vwu']
+_ENDS_WITH = ['bc']
+
+def is_line_ok(line):
+    line = line.strip()
+    line_lw = line.lower()
+    line_len = len(line)
+    #print(line, line_len)
+
+    if line_lw in _WORD_ALLOWED:
+        return True
+
+    # skip wonky mini words
+    if line_len < 4 and _PATTERN_WRONG.search(line):
+        return False
+
+    if line_len < 12:
+        # check for words like 
+        for key, group in itertools.groupby(line):
+            group_len = len(list(group))
+            if key.lower() in ['0', '1', 'x', ' ']: #allow 000, 111, xxx
+                continue
+            if group_len > 2:
+                return False
+
+    if line_len < 7:
+        for group in _BAD_GROUPS:
+            if group in line_lw:
+                return False
+
+    for ew in _ENDS_WITH:
+        if line_lw.endswith(ew):
+            return False
+
+    return True
+
+
+
+def read_line(line, outfile_ok, outfile_ko):
+    line = line.strip("\n")
+    if not line:
+        return
+
+    if is_line_ok(line):
+        outfile_ok.write(line + '\n')
+    else:
+        outfile_ko.write(line + '\n')
+
+
+def read_file(in_name, out_name_ok, out_name_ko):
+    encodings = ['utf-8-sig', 'iso-8859-1']
+    done = False
+    try:
+        for encoding in encodings:
+            try:
+                with open(in_name, 'r', encoding=encoding) as infile, open(out_name_ok, 'w', encoding=encoding) as outfile_ok, open(out_name_ko, 'w', encoding=encoding) as outfile_ko:
+                    for line in infile:
+                        read_line(line, outfile_ok, outfile_ko)
+                    done = True
+                break
+            except UnicodeDecodeError:
+                continue
+    except FileNotFoundError:
+        print("file not found")
+        pass
+
+    if not done:
+        print("couldn't read input file %s (bad encoding?)" % (in_name))
+
+def main():
+    if len(sys.argv) <= 1:
+        print("missing filename")
+        return
+
+    in_name = sys.argv[1]
+
+    base, _ = os.path.splitext(in_name)
+    out_name_ok = base + "_ok.txt"
+    out_name_ko = base + "_ko.txt"
+    read_file(in_name, out_name_ok, out_name_ko)
+
+# #####################################
+
+if __name__ == "__main__":
+    main()
